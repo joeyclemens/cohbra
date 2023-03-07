57146750e969b58978c4d228246e3daa97c488cd
joeyclemens <joeyclemens87@gmail.com>
2023-03-07 16:31:37 +0000

    Update cohbra.py

--- cohbra.py
+++ cohbra.py
@@ -79,4 +79,16 @@ cost = pd.read_csv('costs.csv')
 st.header("Cost table")
 st.write(cost)
 
+# Define the room information as a pandas dataframe
+cost = pd.read_csv('costs.csv')
+
+# Count the number of times 'cohbra' appears in the 'Cost Source Manufacturer' column
+count = len(cost[cost['Cost Source Manufacturer'] == 'Cohbra'])
+
+# Calculate the percentage of rows that have 'cohbra' as the manufacturer
+total_rows = len(cost)
+percentage = (count / total_rows) * 100
+
+# Display the result in Streamlit
+st.write(f"Costs are at {percentage:.2f}% completion.")
 
