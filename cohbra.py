6ab243cf44c519f6cdabf2e2168f231a2141d8c2
Joey <joeyclemens87@gmail.com>
2023-03-10 11:46:27 +0000

    c

--- cohbra.py
+++ cohbra.py
@@ -70,8 +70,9 @@ elif nav == "Costs":
     st.header("Costs")
     # Define the room information as a pandas dataframe
     cost = pd.read_csv('costs.csv')
-    # Count the number of times 'cohbra' appears in the 'Cost Source Manufacturer' column
-    count = len(cost[cost['Cost Source Manufacturer'] == 'Cohbra'])
+    # Count the number of non-null values in the 'Cost Source Manufacturer' column
+count = len(cost['Cost Source Manufacturer'].dropna())
+
     # Calculate the percentage of rows that have 'cohbra' as the manufacturer
     total_rows = len(cost)
     percentage = (count / total_rows) * 100
