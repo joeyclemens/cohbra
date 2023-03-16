b2df9d06e548178b02cb2bc882e0298d78ce4b06
joeyclemens <joeyclemens87@gmail.com>
2023-03-16 12:49:41 +0000

    Update cohbra.py

--- cohbra.py
+++ cohbra.py
@@ -2,84 +2,75 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 import altair as alt
-import numpy as np
+from datetime import datetime
 
 # Set main page title
-st.title("")
+st.title("Progress Dashboard")
+
+# Define function to create the line chart using Altair
+def create_line_chart(df, title):
+    # Convert Dates column to datetime type for plotting purposes
+    df['Dates'] = pd.to_datetime(df['Dates'], format='%d/%m/%Y')
+
+    # Define the line chart using Altair
+    chart = alt.Chart(df).mark_line().encode(
+        x=alt.X('Dates:T', 
+                axis=alt.Axis(title='Date', format=("%d/%m/%Y"), tickCount=len(df.index))),
+        y=alt.Y('Actual', sort=None, title='Targets'),
+        tooltip=[alt.Tooltip('Dates', title='Date'), alt.Tooltip('Actual', title='Target')]
+    ).properties(
+        width=1000  # Set the chart width to 1000 pixels
+    )
+
+    # Add target line to chart
+    target_line = alt.Chart(df).mark_line(strokeDash=[5, 5], stroke='red').encode(
+        x='Dates',
+        y=alt.Y('Target', sort=alt.EncodingSortField(field='Target', order='ascending')),
+    )
+
+    # Combine the two charts
+    final_chart = chart + target_line
+
+    # Set the subtitle of the chart
+    st.subheader(title)
+
+    st.altair_chart(final_chart)
+
+    # Define the header of the table
+    st.header(title)
+    # Display the table
+    st.write(df)
+
+
+
 # Open image
 image = Image.open('MJMEDICAL.png')
 
 # Display image
 st.image(image)
 
-# Load data from CSV file
-df = pd.read_csv('progress.csv')
+# Load data from CSV files
+total_progress = pd.read_csv('total_progress.csv')
+dataentry_progress = pd.read_csv('dataentry_progress.csv')
+roomloading_progress = pd.read_csv('roomloading_progress.csv')
+activity_progress = pd.read_csv('activity_progress.csv')
 
-# Convert Dates column to datetime type for plotting purposes
-df['Dates'] = pd.to_datetime(df['Dates'], format='%d/%m/%Y')
-
-# Define the line chart using Altair
-chart = alt.Chart(df).mark_line().encode(
-    x=alt.X('Dates', title='Date'),
-    y=alt.Y('Actual', sort=None, title='Targets'),
-    tooltip=[alt.Tooltip('Dates', title='Date'), alt.Tooltip('Actual', title='Target')]
-).properties(
-    width=1000  # Set the chart width to 1000 pixels
-)
-
-# Add target line to chart
-target_line = alt.Chart(df).mark_line(strokeDash=[5, 5], stroke='red').encode(
-    x='Dates',
-    y=alt.Y('Target', sort=alt.EncodingSortField(field='Target', order='ascending')),
-)
-
-# Combine the two charts
-final_chart = chart + target_line
-
-# Set the subtitle of the chart
-st.subheader("Progress")
-
-st.altair_chart(final_chart)
+dates = total_progress.Dates.tolist()
+date_format = "%d/%m/%Y"
+parsed_dates = [datetime.strptime(date, date_format) for date in dates]
+total_progress['Dates'] = pd.Series(parsed_dates)
 
 
-# Create a navigation sidebar with options to jump to different sections of the page
-nav = st.sidebar.radio("",["Progress","List of Priority Rooms","Equipment planning, room loading completion and Activities", "Costs"])
+charts = {
+    'Total Progress': total_progress,
+    'Equipment Planning Progress': dataentry_progress,
+    'Room Loading Progress': roomloading_progress,
+    'Activity Progress': activity_progress
+}
 
-# Show the appropriate section based on the selected option
-if nav == "Progress":
-    # Define the header of the table
-    st.header("Progress")
-    # Define the room information as a pandas dataframe
-    progress = pd.read_csv('progress.csv')
-    # Display the table
-    st.write(progress)
-elif nav == "List of Priority Rooms":
-    # Define the header of the table
-    st.header("List of Priority Rooms")
-    # Define the room information as a pandas dataframe
-    rooms = pd.read_csv('priority.csv')
-    # Display the table
-    st.write(rooms)
-elif nav == "Equipment planning, room loading completion and Activities":
-    # Define the room information as a pandas dataframe
-    datroom = pd.read_csv('dataent.csv')
-    st.header("Equipment planning, room loading completion and Activities")
-    st.write(datroom)
-elif nav == "Costs":
-    # Define the header of the table
-    st.header("Costs")
-    # Define the room information as a pandas dataframe
-    cost = pd.read_csv('costs.csv')
-    # Count the number of times 'cohbra' appears in the 'Cost Source Manufacturer' column
-    count = len(cost[cost['Cost Source Manufacturer'] == 'Craven'])
-    # Calculate the percentage of rows that have 'cohbra' as the manufacturer
-    total_rows = len(cost)
-    percentage = (count / total_rows) * 100
-    # Display the percentage of costs for the chosen manufacturer
-    st.write(f"Costs are at {percentage:.2f}% completion.")
-    # Display the table
-    st.write(cost)
+# Add selectbox to choose which graph and table to show
+chart_choice = st.sidebar.selectbox('Choose chart', list(charts.keys()))
 
-   
+selected_chart = charts[chart_choice]
 
-
+create_line_chart(selected_chart, chart_choice)
