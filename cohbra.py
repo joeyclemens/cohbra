3629695bb8071dba2d2426c1ce0fd272fd322f5b
joeyclemens <joeyclemens87@gmail.com>
2023-04-18 09:20:56 +0100

    s

--- cohbra.py
+++ cohbra.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 
 
+
 # Set main page title
 st.title("KPI Dashboard")
 
@@ -86,7 +87,7 @@ tables = {
 # Add textbox to sidebar
 st.sidebar.markdown("<u>Links:</u>", unsafe_allow_html=True)
 # Add hyperlink to sidebar
-st.sidebar.write("[KPI Tracker](https://mjmedical.sharepoint.com/:x:/s/COHBRA/Eb4o_yhqVspHjEDxC7hptYYBy4ryOXYl8nmXwHW0wT12Vw?e=tzJZid)")
+st.sidebar.write('<a href="https://mjmedical.sharepoint.com/:x:/s/COHBRA/Eb4o_yhqVspHjEDxC7hptYYBy4ryOXYl8nmXwHW0wT12Vw?e=tzJZid" style="color: orange; text-decoration: none;">KPI Tracker</a>', unsafe_allow_html=True)
 # Add a dividing line
 st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 
@@ -125,5 +126,6 @@ elif option == 'Tables':
 # Add a dividing line
 st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 # Add textbox to sidebar
-st.sidebar.markdown("<u>Areas needing most attention:</u>\n- Specs", unsafe_allow_html=True)
- 
+st.sidebar.markdown("<u>Areas needing most attention:</u>", unsafe_allow_html=True)
+# Add hyperlink to sidebar
+st.sidebar.write('<a href="https://mjmedical.sharepoint.com/:x:/s/COHBRA/Eb4o_yhqVspHjEDxC7hptYYBy4ryOXYl8nmXwHW0wT12Vw?e=tMaVJw&nav=MTVfezc5MUFEMjVBLTdDMUMtNEJERS1CRjU1LUM0QUMxQjgxQzcwRX0" style="color: orange; text-decoration: none;">• Specs</a>', unsafe_allow_html=True)
